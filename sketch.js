581ab2b12203b71124dc9911ed2291977e518ab5
Nicolas Schlecker <nicolasschlecker@gmail.com>
2018-01-12 14:19:48 +0100

    Changed visualisation from 3D to 2D
    Switched from 3D to 2D rendering for better visuals and fixed some posititioning and sizing problems.

--- sketch.js
+++ sketch.js
@@ -1,97 +1,124 @@
 var source, destination, auxiliary;
-var colors = ['blue', 'red', 'yellow', 'green', 'gray', 'magenta', 'purple', 'lightblue', 'pink'];
-var ch, cw, mcw, stackSize, stepTime;
+var colors = ['blue', 'red', 'darkgrey', 'green', 'grey', 'magenta', 'purple', 'lightblue', 'darkred', 'darkblue'];
+var stackSize, stepTime;
 
+var dW, dH;
+
+// p5 setup function
 function setup() {
-    createCanvas(windowWidth, 600, WEBGL);
+    createCanvas(windowWidth, 600);
 
-    startHanoi(5, 500);
+    // Default: initialize with 5 disks and 500ms per step
+    start(5, 500);
 }
 
-window.onresize = function() {
+// Recalculate values / Resize canvas on window resize
+window.addEventListener('resize', () => {
     resizeCanvas(windowWidth, windowHeight);
-    calcWidths();
-};
+    calcSizes();
+});
 
+// p5 draw function
 function draw() {
     background(255);
-    noStroke();
 
-    translate(-width / 2, -height / 2);
-    directionalLight(255, 255, 255, -2, -2, 1);
-    
-    var w = width - width / 10;
-    var a = width / 10 / 2;
-    drawStack(source,  a + (w / 3) - mcw);
-    drawStack(auxiliary, a + (w / 3) * 2 - mcw);
-    drawStack(destination, a + w - mcw);
+    // Helper variables for positioning
+    var s = width / 10;
+    var w = width - s;
+    var w3 = w / 3;
+
+    // Draw Base & Poles
+    fill('brown');
+    rect(w3 - 2.5 * s, height - dH * (stackSize - 1), w, dH * 1.3, 20);
+    rect(w3 - s - dH / 2, height - dH * stackSize - dH * stackSize, dH, dH * (stackSize + 1), 10, 10, 0, 0);
+    rect(w3 * 2 - s - dH / 2, height - dH * stackSize  - dH * stackSize, dH, dH * (stackSize + 1), 10, 10, 0, 0);
+    rect(w - s - dH / 2, height - dH * stackSize - dH * stackSize, dH, dH * (stackSize + 1), 10, 10, 0, 0);
+
+    // Draw Stacks
+    drawStack(source, w3 - s); // Source
+
+    if(stackSize % 2 != 0) {
+        drawStack(auxiliary, w3 * 2 - s); // Auxiliary
+        drawStack(destination, w - s); // Destination
+    } else {
+        drawStack(destination, w3 * 2 - s); // Auxiliary
+        drawStack(auxiliary, w - s); // Destination
+    }
 }
 
-function startHanoi(sz, st) {
+function start(sz, st) {
+    // Initialize / Clear stacks
     source = [];
     destination = [];
     auxiliary = [];
 
-    if(sz > 10)
+    if (sz > 10)
         throw 'Max Stacksize is 10!';
 
+    // Setup variables
     stackSize = sz;
     stepTime = st;
-    calcWidths();
+    calcSizes();
 
-    for(var i = stackSize; i > 0; i--)
-        source.push({ value: i, color: colors[i] });
+    // Fill source stack with appropriate amount of disks
+    for (var i = stackSize; i > 0; i--) {
+        console.log(colors[i - 1]);
+        source.push({
+            value: i,
+            color: colors[i - 1]
+        });
+    }
 
-    hanoi(source, destination, auxiliary);
+    // Start the solving process
+    hanoi();
 }
 
-function calcWidths() {
-    cw = (width / 3 - (width / 6)) / stackSize;
-    mcw = stackSize * cw;
-    ch = width / 40;
+// Calculate disk sizes based on the window width
+function calcSizes() {
+    dW = width / 5 / stackSize;
+    dH = dW / 3 * 1.4;
 }
 
+// Draw all disks in a given stack at a given location
 function drawStack(stack, x) {
-    push();
-
-    translate(x, height - 100, -mcw / 2);
-
-    push();
-    fill('brown');
-    translate(0, - ch * 2 - 10);
-    cylinder(cw / 4, ch * stackSize + 10);
-    pop();
-
+    var counter = 0;
     stack.forEach(function (element) {
-        fill(element.color);
-        cylinder(cw * element.value / 2, ch, 48, 32);
-        translate(0, -ch, 0);
-    })
+        let eW = dW * element.value;
 
-    pop();
+        fill(element.color);
+        rect(x - eW / 2, height - dH * stackSize - counter * dH, eW, dH, 20);
+
+        fill('white');
+        textSize(dH - 2);
+        textAlign(CENTER);
+        text(element.value, x - eW / 2, height - dH * stackSize - counter * dH + 1, eW, dH);
+        counter++;
+    })
 }
 
 function hanoi() {
+    // Calculate total moves required to solve
     var totalMoves = Math.pow(2, source.length);
 
+    // Execute step function as often as required
     var loop = function (i) {
         step(i, function () {
             i += 1;
-            if (i < totalMoves)
-                loop(i);
+            if (i < totalMoves) loop(i);
         });
     }
     loop(1);
 }
 
+// Do one step in solving
 function step(i, callback) {
     setTimeout(function () {
+
+        // Execute legal movement check
         if (i % 3 == 1)
             move(source, destination);
-
         else if (i % 3 == 2)
             move(source, auxiliary);
-
         else if (i % 3 == 0)
             move(auxiliary, destination);
 
@@ -99,19 +126,20 @@ function step(i, callback) {
     }, stepTime);
 }
 
+// Check for legal movement & execute
 function move(source, destination) {
-    var pole1TopDisk = source.pop();
-    var pole2TopDisk = destination.pop();
+    var disk1 = source.pop();
+    var disk2 = destination.pop();
 
-    if (!pole1TopDisk) {
-        source.push(pole2TopDisk);
-    } else if (!pole2TopDisk) {
-        destination.push(pole1TopDisk);
-    } else if (pole1TopDisk.value > pole2TopDisk.value) {
-        source.push(pole1TopDisk);
-        source.push(pole2TopDisk);
+    if (!disk1) {
+        source.push(disk2);
+    } else if (!disk2) {
+        destination.push(disk1);
+    } else if (disk1.value > disk2.value) {
+        source.push(disk1);
+        source.push(disk2);
     } else {
-        destination.push(pole2TopDisk);
-        destination.push(pole1TopDisk);
+        destination.push(disk2);
+        destination.push(disk1);
     }
 }
